4784348614ea13a45379caa84d45f43ed5a5c64c
Jiawei Lu <jl5999@columbia.edu>
2023-07-30 22:55:42 -0400

    spring hateoas

--- src/main/java/com/javey/springbootreview/controller/Home.java
+++ src/main/java/com/javey/springbootreview/controller/Home.java
@@ -1,6 +1,9 @@
 package com.javey.springbootreview.controller;
 
 import com.javey.springbootreview.exception.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -8,9 +11,17 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/")
 public class Home {
-    @GetMapping("hello")
+    private MessageSource messageSource;
+
+    @Autowired
+    public Home(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
+    @GetMapping(value = "home")
     public String home() {
-        return "Hello World!";
+        return messageSource.getMessage("home.message", null, "Home",
+                LocaleContextHolder.getLocale());
     }
 
     @GetMapping("hello/{name}")
--- src/main/java/com/javey/springbootreview/controller/UserController.java
+++ src/main/java/com/javey/springbootreview/controller/UserController.java
@@ -1,21 +1,36 @@
 package com.javey.springbootreview.controller;
 
 import com.javey.springbootreview.entity.UserEntity;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
+    @GetMapping()
+    public List<UserEntity> getAllUsers() {
+        return new ArrayList<>();
+    }
+
     @PostMapping()
     public ResponseEntity<String> addUser(@Valid @RequestBody UserEntity user) {
         System.out.println(user);
         return new ResponseEntity<>("ok", HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public EntityModel<UserEntity> getUserById(@PathVariable String id) {
+        EntityModel<UserEntity> userEntityModel = EntityModel.of(new UserEntity(id, "john", "123@gmail.com"));
+        WebMvcLinkBuilder link = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).getAllUsers());
+        userEntityModel.add(link.withRel("all-users"));
+
+        return userEntityModel;
     }
 }
